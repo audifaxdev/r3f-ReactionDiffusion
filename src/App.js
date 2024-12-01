b8862afc6f107e1941666820ad1f723d0e9fe3c2
Kevin Barreau <audifaxdev@gmail.com>
2024-12-01 14:40:05 +1300

    basic reaction diffusion ping pong shader with react three fiber workflow

--- src/App.js
+++ src/App.js
@@ -7,15 +7,23 @@ import {
   createPortal,
   extend
 } from "@react-three/fiber";
-import {OrthographicCamera, useFBO, shaderMaterial, useTexture, PerspectiveCamera} from "@react-three/drei";
+import {
+  OrthographicCamera,
+  useFBO,
+  shaderMaterial,
+  useTexture,
+  PerspectiveCamera
+} from "@react-three/drei";
 import * as THREE from "three";
+import {Clock} from 'three';
 
 const OffScreenMaterial = shaderMaterial(
   {
     debugUVTxt: { value: null },
     bufferTexture: { value: null },
     res: { value: new THREE.Vector2(0, 0) },
-    time: 0,
+    iTime: 0,
+    iTimeDelta: 0,
     smokeSource: { value: new THREE.Vector3(0, 0, 0) }
   },
   // vertex shader
@@ -31,36 +39,76 @@ const OffScreenMaterial = shaderMaterial(
     #define DEBUG 0
   
   
-    uniform float time;//The width and height of our screen
+    uniform float iTimeDelta;//The width and height of our screen
+    uniform float iTime;//The width and height of our screen
     uniform vec2 res;//The width and height of our screen
     uniform sampler2D bufferTexture;//Our input texture
     uniform sampler2D debugUVTxt;
     uniform vec3 smokeSource;//The x,y are the posiiton. The z is the power/density
     varying vec2 vUv;
+    
+    const vec2 DiffusionRate = vec2(1.0, 0.5);
+    const float KillRate = 0.062;
+    const float FeedRate = 0.0545;
+    const float Speed = 40.0;
+    vec2 center = vec2(0.5, 0.5);
+    float radius = .25;
+    const float thickness = .015;
+    
+    // vec3 iResolution = vec3(res, 2.);
+    
+    vec2 computeLaplacian(vec2 uv, vec2 current)
+    {
+        vec3 iResolution = vec3(res, 2.);
+    
+        vec2 pixelSize = vec2(2.) / res;
+        
+        // with diagonals.
+        return (texture(bufferTexture, uv + vec2(pixelSize.x, 0.0)).xy +
+                texture(bufferTexture, uv - vec2(pixelSize.x, 0.0)).xy +
+                texture(bufferTexture, uv + vec2(0.0, pixelSize.y)).xy +
+                texture(bufferTexture, uv - vec2(0.0, pixelSize.y)).xy) * 0.2
+                +
+               (texture(bufferTexture, uv + pixelSize).xy +
+                texture(bufferTexture, uv - pixelSize).xy +
+                texture(bufferTexture, uv + vec2(pixelSize.x, -pixelSize.y)).xy +
+                texture(bufferTexture, uv - vec2(pixelSize.x, -pixelSize.y)).xy) * 0.05
+        -
+                current;
+    }
 
     void main() {
-      vec2 pixel = vUv;
-      gl_FragColor = texture2D( bufferTexture, gl_FragCoord.xy / res.xy );
-
-      //Get the distance of the current pixel from the smoke source
-      float dist = distance(pixel * res.xy, smokeSource.xy * res.xy);
-
-      gl_FragColor.rgb += smokeSource.z * max(10.0 - dist, 0.0) ;
-
-      //Smoke diffuse
-      vec4 rightColor = texture2D(bufferTexture,vec2(pixel.x + 1.0/res.x,pixel.y));
-      vec4 leftColor = texture2D(bufferTexture,vec2(pixel.x - 1.0/res.x,pixel.y));
-      vec4 upColor = texture2D(bufferTexture,vec2(pixel.x,pixel.y + 1.0/res.y));
-      vec4 downColor = texture2D(bufferTexture,vec2(pixel.x,pixel.y - 1.0/res.y));
-
-      //Diffuse equation
-      float factor = 8.0 * 0.016 * (leftColor.r + rightColor.r + downColor.r * 3.0 + upColor.r - 6.0 * gl_FragColor.r);
-
-      //Account for low precision of texels
-      float minimum = 0.000003;
-      if (factor >= -minimum && factor < -0.10) factor = -minimum;
-
-      gl_FragColor.rgb += factor;
+      vec3 iResolution = vec3(res, 2.);
+      vec2 fragCoord = gl_FragCoord.xy;
+      vec2 uv = vUv;
+    
+      if (iTime < 0.1) {
+        vec2 toMid = (res * 0.5 - fragCoord) / res.y;
+        toMid += sin(atan(toMid.x, toMid.y)*20.0) * 0.01; // Wobble circle a bit to get the desired effects faster.
+        float midDistSq = dot(toMid, toMid);
+        float initVal = pow(sin(midDistSq * 40.0) * 0.5 + 0.5, 5.0);
+        
+        gl_FragColor = vec4(1.0, initVal, 0.0, 1.0);
+        return;
+      }
+      
+      vec2 current = clamp(texture(bufferTexture, uv).xy, vec2(0.), vec2(1.));
+      
+      // Compute diffusion.
+      vec2 laplacian = computeLaplacian(uv, current);
+      vec2 diffusion = DiffusionRate * laplacian;
+          
+      // Compute reaction.
+      float u = current.x;
+      float v = current.y;    
+      float reactionU = - u * v * v + FeedRate * (1. - u);
+      float reactionV = u * v * v - (FeedRate + KillRate) * v;
+      
+      // Apply using simple forward Euler.
+      vec2 newValues = current + (diffusion + vec2(reactionU, reactionV)) * Speed * iTimeDelta;
+      
+      
+      gl_FragColor = vec4(newValues, 0.0, 1.0);
     }
   `
 );
@@ -71,11 +119,21 @@ const OffScreenScene = forwardRef(function OffScreenScene(props, ref) {
   const { size } = useThree();
   const offscreenMat = useRef(null);
   const debugUVTxt =  useTexture("/uvdebugtiles.png");
+  const clock = new Clock();
+
+
   useFrame(() => {
+    if (!clock.running) {
+      clock.start();
+    }
     if (offscreenMat.current) {
       // console.log('offscreenMat.current.uniforms', offscreenMat.current.uniforms)
-      // console.log('offscreenMat.current.uniforms.time.value', offscreenMat.current.uniforms.time.value)
-      offscreenMat.current.uniforms.time.value += 0.01;
+      // console.log('offscreenMat.current.uniforms', {
+      //   time: offscreenMat.current.uniforms.iTime.value,
+      //   delta: offscreenMat.current.uniforms.iTimeDelta.value
+      // })
+      offscreenMat.current.uniforms.iTimeDelta.value = clock.getDelta();
+      offscreenMat.current.uniforms.iTime.value = clock.getElapsedTime();
     }
   });
 
@@ -87,7 +145,7 @@ const OffScreenScene = forwardRef(function OffScreenScene(props, ref) {
           debugUVTxt={debugUVTxt}
           ref={offscreenMat}
           bufferTexture={props.map}
-          res={new THREE.Vector2(1024, 1024)}
+          res={new THREE.Vector2(size.width, size.height)}
           smokeSource={new THREE.Vector3(0, 0, 0)}
         />
       </mesh>
@@ -101,11 +159,11 @@ const Plane = () => {
   const offScreen = useRef();
   const onScreen = useRef();
 
-  const offScreenFBOTexture = useFBO(1024, 1024, {
+  const offScreenFBOTexture = useFBO(size.width, size.height, {
     minFilter: THREE.LinearFilter,
     magFilter: THREE.NearestFilter
   });
-  const onScreenFBOTexture = useFBO(1024, 1024, {
+  const onScreenFBOTexture = useFBO(size.width, size.height, {
     minFilter: THREE.LinearFilter,
     magFilter: THREE.NearestFilter
   });
